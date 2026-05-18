f958e93d5cd0e7821c6b35c1c4a28c6a12eb5d41
Dulce-Villafana <dulce.villafana.esc@gmail.com>
2026-05-18 12:04:33 -0600

    updateservices

--- src/service/ContactoService.java
+++ src/service/ContactoService.java
@@ -1,4 +1,28 @@
 package service;
 
+import model.Contacto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactoService {
+        private final ArrayList<Contacto> contactos=new ArrayList<>();
+        //---Metodo para agregar contacto a la lista
+
+        //---Verificar si un contacto existe en la lista
+
+        //--Mostrar la lista de contactos
+        public List<Contacto> getContactos(){
+        return contactos;
+    }
+        //--Buscar Contacto(String nombre,String Apellido)
+
+        //--Eliminar contacto
+
+        //--Modificar telefono
+
+        //--Agenda llena
+
+        //--Espacios libres
+
 }
