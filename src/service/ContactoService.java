68a4fe136f844cf441e4b2afa511731f9f1b39c3
olafmartinez <martineztorres1725@gmail.com>
2026-05-18 12:41:10 -0600

    feat(agenda): agrego funcionalidades de contactos

--- src/service/ContactoService.java
+++ src/service/ContactoService.java
@@ -7,21 +7,83 @@ import java.util.List;
 
 public class ContactoService {
         private final ArrayList<Contacto> contactos=new ArrayList<>();
-        //---Metodo para agregar contacto a la lista
+
+    private final int maxContactos;
+
+    public ContactoService() {
+        this.maxContactos = 10;
+    }
+    public ContactoService(int maxContactos) {
+
+        this.maxContactos = maxContactos;
+    }
+
+    //---Metodo para agregar contacto a la lista
+
+    public void addContacto(Contacto contacto) {
+
+        // VALIDAR CAMPOS VACIOS
+        if(contacto.getFirstName().trim().isEmpty()
+                ||
+                contacto.getLastName().trim().isEmpty()) {
+
+            System.out.println(
+                    "Nombre y apellido obligatorios");
+
+            return;
+        }
+
+        if(agendaLlena()) {
+
+            System.out.println(
+                    "Agenda llena");
+
+            return;
+        }
+
+        if(existeContacto(contacto)) {
+
+            System.out.println(
+                    "El contacto ya existe");
+
+            return;
+        }
+
+        contactos.add(contacto);
+
+        System.out.println(
+                "Contacto agregado");
+    }
 
         //---Verificar si un contacto existe en la lista
 
-        //--Mostrar la lista de contactos
-        public List<Contacto> getContactos(){
-        return contactos;
-    }
-        //--Buscar Contacto(String nombre,String Apellido)
+        public boolean existeContacto(
+                Contacto contacto) {
 
-        //--Eliminar contacto
+            for (Contacto c : contactos) {
 
-        //--Modificar telefono
+                if (c.getFirstName()
+                        .equalsIgnoreCase(
+                                contacto.getFirstName())
+                        &&
+                        c.getLastName()
+                                .equalsIgnoreCase(
+                                        contacto.getLastName())) {
 
-        //--Agenda llena
+                    return true;
+                }
+            }
+            //--Mostrar la lista de contactos
+            public List<Contacto> getContactos () {
+                return contactos;
+            }
+            //--Buscar Contacto(String nombre,String Apellido)
+
+            //--Eliminar contacto
+
+            //--Modificar telefono
+
+        }        //--Agenda llena
 
         //--Espacios libres
 
