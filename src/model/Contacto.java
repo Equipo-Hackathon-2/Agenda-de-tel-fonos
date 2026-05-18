553762edcd15e8ad0d3284ad1a5ad3d344567e3a
Dulce-Villafana <dulce.villafana.esc@gmail.com>
2026-05-18 10:58:57 -0600

    Initial commit

---
+++ src/model/Contacto.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Contacto {
+    private String firstName;
+    private String lastName;
+    private long contactNumber;
+
+    public Contacto(String firstName, String lastName, long contactNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNumber = contactNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", contactNumber=" + contactNumber +
+                '}';
+    }
+}
